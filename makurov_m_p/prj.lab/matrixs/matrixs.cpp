cb808814e85781e2aab580df3fc9d6b1ab9bf324
Mikhail Makurov/m1n1tea <113550126+m1n1tea@users.noreply.github.com>
2023-04-10 14:45:50 +0300

    Add files via upload

--- makurov_m_p/prj.lab/matrixs/matrixs.cpp
+++ makurov_m_p/prj.lab/matrixs/matrixs.cpp
@@ -1,5 +1,7 @@
 ﻿#include "include/matrixs/matrixs.hpp"
 MatrixS::MatrixS(const std::ptrdiff_t size_y, const std::ptrdiff_t size_x) : size_x_(size_x), size_y_(size_y), matrix_(ArrayT<int>(size_x* size_y)), rows_(ArrayT<std::ptrdiff_t>(size_y)) {
+    sizeCheck(size_y_+1);
+    sizeCheck(size_x_+1);
     for (std::ptrdiff_t i = 1; i < size_y_; ++i) {
         rows_[i] = i;
     }
@@ -8,7 +10,9 @@ MatrixS::MatrixS(const std::ptrdiff_t size_y, const std::ptrdiff_t size_x) : siz
 
 MatrixS::MatrixS(const SizeType& size) :size_x_(get<1>(size)), size_y_(get<0>(size)), matrix_(ArrayT<int>(get<0>(size)* get<1>(size))), rows_(ArrayT<std::ptrdiff_t>(get<0>(size)))
 {
-    for (std::ptrdiff_t i = 1; i < get<0>(size); ++i) {
+    sizeCheck(size_y_+1);
+    sizeCheck(size_x_+1);
+    for (std::ptrdiff_t i = 1; i < size_y_; ++i) {
         rows_[i] = i;
     }
 }
@@ -53,13 +57,13 @@ void MatrixS::resize(const SizeType& elem)
 void MatrixS::resize(const std::ptrdiff_t size_y_new, const std::ptrdiff_t size_x_new) {
     sizeCheck(size_y_new);
     sizeCheck(size_x_new);
-    std::ptrdiff_t sizeXSmall = (size_x_new < size_x_) ? size_x_new : size_x_;
-    std::ptrdiff_t sizeYSmall = (size_y_new < size_y_) ? size_y_new : size_y_;
+    std::ptrdiff_t size_x_small = (size_x_new < size_x_) ? size_x_new : size_x_;
+    std::ptrdiff_t size_y_small = (size_y_new < size_y_) ? size_y_new : size_y_;
     ArrayT<int> newMatrix = ArrayT<int>(size_x_new * size_y_new);
 
 
-    for (std::ptrdiff_t y = 0; y < sizeYSmall; ++y) {
-        for (std::ptrdiff_t x = 0; x < sizeXSmall; ++x) {
+    for (std::ptrdiff_t y = 0; y < size_y_small; ++y) {
+        for (std::ptrdiff_t x = 0; x < size_x_small; ++x) {
             newMatrix[y * size_x_new + x]= matrix_[rows_[y] * size_x_ + x];
         }
     }
